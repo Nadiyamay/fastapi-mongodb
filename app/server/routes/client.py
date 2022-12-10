fdb4b165110eb54e7f89ec6523d39f5757278318
Nadiyamay <ya.nda9301@gmail.com>
2022-12-10 15:02:10 +0400

    add ability to retrieve a client and all clients

--- app/server/routes/client.py
+++ app/server/routes/client.py
@@ -22,3 +22,18 @@ async def add_client_data(client: ClientSchema = Body(...)):
     client = jsonable_encoder(client)
     new_client = await add_client(client)
     return ResponseModel(new_client, "Client added successfully.")
+
+@router.get("/", response_description="Clients retrieved")
+async def get_clients():
+    clients = await retrieve_clients()
+    if clients:
+        return ResponseModel(clients, "Clients data retrieved successfully")
+    return ResponseModel(clients, "Empty list returned")
+
+
+@router.get("/{id}", response_description="Client data retrieved")
+async def get_client_data(id):
+    client = await retrieve_client(id)
+    if client:
+        return ResponseModel(client, "Client data retrieved successfully")
+    return ErrorResponseModel("An error occurred.", 404, "Client doesn't exist.")
